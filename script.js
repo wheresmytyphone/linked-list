25b0a833f844a100226f9a93f6cb19841913ce1d
Travis Rollins <TROLL59910@aol.com>
2017-05-14 16:59:24 -0600

    Add website card in javascript

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 $('.submit').on('click', function(event) {
   event.preventDefault();
-  var websiteTitle = $('#title').val();
-  var websiteUrl = $('#url').val();
-  $('.web-title').text(websiteTitle);
-  $('.web-url').text(websiteUrl);
+  var $urlTitle = $('#title').val();
+  var $url = $('#url').val();
+  var urlCard = new urlCard($urlTitle, $url);
+  addCard(urlCard);
 });
+
+function urlCard (title, url) {
+  this.title = title;
+  this.url = url;
+  console.log(this);
+}
+
+
+function addCard(urlCard) {
+  var websiteTitle = urlCard.title;
+  var websiteUrl = urlCard.url
+  $('aside').prepend('<div class="linked-card"><h3 class="web-title">${$websiteTitle}</h3><hr><p class="web-url">${$websiteUrl}</p><hr><div class="button-container"><p class="read">Read</p><p class="read">Delete</p></div></div>');
+}
